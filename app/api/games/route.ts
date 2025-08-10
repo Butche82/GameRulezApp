d10e40cb139837d523adfa0f32aa48d2bf806674
Butcha82 <lee.butcher1982@gmail.com>
2025-08-10 08:11:35 +0200

    Update route.ts

--- app/api/games/route.ts
+++ app/api/games/route.ts
@@ -1,8 +1,8 @@
 export const runtime2 = "nodejs";
-import { NextResponse as NextResponse2 } from "next/server";
-import { gamesDb } from "@/lib/vector";
+import { NextResponse } from "next/server";
+import { gamesDb } from "../../../lib/vector";
 
 export async function GET() {
   const games = Object.values(gamesDb).sort((a, b) => a.title.localeCompare(b.title));
-  return NextResponse2.json({ games });
+  return NextResponse.json({ games });
 }
